ebcb408c9c53711645da9bb0f63150962ff80195
James Socol <james@mozilla.com>
2011-07-22 17:30:31 -0400

    Y U FAIL?

--- waffle/helpers.py
+++ waffle/helpers.py
@@ -5,12 +5,35 @@ from waffle import flag_is_active, sample_is_active, switch_is_active
 
 
 @jinja2.contextfunction
-def flag_helper(context, flag_name):
-    return flag_is_active(context['request'], flag_name)
+def flag_helper(context, name, output=None):
+    active = flag_is_active(context['request'], name)
+    if output is None:
+        return active
+    elif active:
+        return output
+    return u''
+
+
+def sample_helper(name, output=None):
+    active = sample_is_active(name)
+    if output is None:
+        return active
+    elif active:
+        return output
+    return u''
+
+
+def switch_helper(name, output=None):
+    active = switch_is_active(name)
+    if output is None:
+        return active
+    elif active:
+        return output
+    return u''
 
 
 jingo.env.globals['waffle'] = {
     'flag': flag_helper,
-    'switch': switch_is_active,
-    'sample': sample_is_active,
+    'switch': switch_helper,
+    'sample': sample_helper,
 }
--- waffle/tests/test_templates.py
+++ waffle/tests/test_templates.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AnonymousUser
 
+import mock
 from test_utils import RequestFactory, TestCase
 
 from test_app import views
+import waffle
 from waffle.middleware import WaffleMiddleware
 
 
@@ -31,3 +33,17 @@ class WaffleTemplateTests(TestCase):
         self.assertContains(response, 'flag off')
         self.assertContains(response, 'switch off')
         self.assertContains(response, 'sample')
+
+    @mock.patch.object(waffle, 'flag_is_active')
+    @mock.patch.object(waffle, 'switch_is_active')
+    @mock.patch.object(waffle, 'sample_is_active')
+    def test_jingo_sugar(self, sample_is_active, switch_is_active,
+                         flag_is_active):
+        flag_is_active.return_value = True
+        switch_is_active.return_value = True
+        sample_is_active.return_value = True
+        request = get()
+        response = process_request(request, views.flag_in_jingo)
+        self.assertContains(response, 'flag sugar on')
+        self.assertContains(response, 'switch sugar on')
+        self.assertContains(response, 'sample sugar on')
